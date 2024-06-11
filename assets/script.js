f28f2473baa5d039860f110012761f98d673844f
Emma Gaxiola <emma.gaxiol@gmail.com>
2024-06-11 13:21:42 -0600

    Creacion de header, script de opcion multiple, cambio de imagen.

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,27 @@ function phoneMask() {
     $(this).val(num.substring(0,1) + '(' + num.substring(1,4) + ')' + num.substring(4,7) + '-' + num.substring(7,11)); 
 }
 $('[type="tel"]').keyup(phoneMask);
+
+function hideShowDiv(val) {
+  
+  if (val==2) {
+    document.getElementById('show-me').style.display='block';
+  }
+  else {
+    document.getElementById('show-me').style.display='none';
+  }
+
+  if (val==1) {
+    document.getElementById('calle').style.display='block';
+  }
+  else {
+    document.getElementById('calle').style.display='none';
+  }
+
+  if (val==3) {
+    document.getElementById('contacto').style.display='block';
+  }
+  else {
+    document.getElementById('contacto').style.display='none';
+  }
+}   
